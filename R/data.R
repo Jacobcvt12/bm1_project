6001149e948e257187753fa03a08e55f7ff6110e
Jacob Carey <jacobcvt12@gmail.com>
2015-02-26 15:48:57 -0500

    Start script to simulate data

---
+++ R/data.R
@@ -0,0 +1,10 @@
+create.data <- function(params,
+                        dir,
+                        distribution=rnorm, 
+                        mix.prob=c(0.5, 0.5), 
+                        obs=100)
+{
+    k <- length(mix.prob)
+
+}
+
