9552ed8e17383bcc455b9a226d0d4cdcf32ba7d7
Jacob Carey <jacobcvt12@gmail.com>
2015-03-01 09:32:17 -0500

    Rewrite inital value generation. Continue loop.

--- src/sampler.cpp
+++ src/sampler.cpp
@@ -15,32 +15,46 @@ Rcpp::NumericMatrix sampler(Rcpp::NumericVector y,
     Rcpp::RNGScope scope;
 
     // simulate first value from prior distribution
-    double p = Rcpp::as<double>(Rcpp::rbeta(1, a, b));
+    double p = R::rbeta(a, b);
     PHI(0, 0) = p;
 
-    double theta1= Rcpp::as<double>(Rcpp::rnorm(1, mu0(0), sqrt(tau20(0))));
+    double theta1= R::rnorm(mu0(0), sqrt(tau20(0)));
     PHI(0, 1) = theta1;
 
-    double s1 = Rcpp::as<double>(Rcpp::rgamma(1, v0(0)/2, 
-                                              2/(v0(0)*sigma20(0))));
+    double s1 = R::rgamma(v0(0)/2, 2/(v0(0)*sigma20(0)));
     PHI(0, 2) = s1;
 
-    double theta2 = Rcpp::as<double>(Rcpp::rnorm(1, mu0(1), sqrt(tau20(1))));
+    double theta2 = R::rnorm(mu0(1), sqrt(tau20(1)));
     PHI(0, 3) = theta2;
 
-    double s2 = Rcpp::as<double>(Rcpp::rgamma(1, v0(1)/2, 
-                                              2/(v0(1)*sigma20(1))));
+    double s2 = R::rgamma(v0(1)/2, 2/(v0(1)*sigma20(1)));
     PHI(0, 4) = s1;
 
     // still need to initialize xi's
 
     // initialize value for delta
-    double delta = 1;
-    
+    double delta2 = 1;
+
     // begin metropolis routine
     for (int s = 1; s < S; ++s) {
-        double p = PHI(s-1, 0);
-        double p_star = Rcpp::as<double>(Rcpp::rnorm(1, p, delta^2));
+        // Get last iteration estimation
+        p = PHI(s-1, 0);
+        theta1 =  PHI(s-1, 1);
+        s1 = PHI(s-1, 2);
+        theta2 = PHI(s-1, 3);
+        s2 = PHI(s-1, 4);
+
+        // compute candidate values
+        double p_star = R::rnorm(p, delta2);
+        double theta1_star = R::rnorm(theta1, delta2);
+        double s1_star = R::rnorm(s1, delta2);
+        double theta2_star = R::rnorm(theta2, delta2);
+        double s2_star = R::rnorm(s2, delta2);
+
+        // accept or reject candidate
+        // likeiihood
+        
+        // r
     }
 
     return PHI;
