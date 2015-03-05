b0f17123d590b49f2fb2815949693e4c6e71a41c
Tharper84 <Tharper84@Yahoo.com>
2015-03-05 14:29:02 -0500

    Update ChainSummary.R

--- R/ChainSummary.R
+++ R/ChainSummary.R
@@ -1,23 +1,52 @@
-### chain <- 1 chain of MCMC output with columns Mean, Var, K
+### chain <- 1 chain of MCMC output with columns $P, $Theta1, $Theta2, $s1, $s2, $x1, $x2
 
 library(coda)
 
-MEAN_auto_corr<-acf(chain$Mean, plot=FALSE)
-MEAN_EffectiveSS<-effectiveSize(chain$Mean)
-MEAN_HPD<-HPDinterval(as.mcmc(chain$Mean), prob=0.95)
+P_auto_corr<-acf(chain$P, plot=FALSE)
+P_EffectiveSS<-effectiveSize(chain$P)
+P_HPD<-HPDinterval(as.mcmc(chain$P), prob=0.95)
 
-VAR_auto_corr<-acf(chain$Var, plot=FALSE)
-VAR_EffectiveSS<-effectiveSize(chain$Var)
-VAR_HPD<-HPDinterval(as.mcmc(chain$Var), prob=0.95)
+THETA1_auto_corr<-acf(chain$Theta1, plot=FALSE)
+THETA1_EffectiveSS<-effectiveSize(chain$Theta1)
+THETA1_HPD<-HPDinterval(as.mcmc(chain$Theta1), prob=0.95)
 
-K_Table<-table(chain$K)
+S1_auto_corr<-acf(chain$S1, plot=FALSE)
+S1_EffectiveSS<-effectiveSize(chain$S1)
+S1_HPD<-HPDinterval(as.mcmc(chain$S1), prob=0.95)
 
-MEAN_auto_corr
-MEAN_EffectiveSS
-MEAN_HPD
+THETA2_auto_corr<-acf(chain$Theta2, plot=FALSE)
+THETA2_EffectiveSS<-effectiveSize(chain$Theta2)
+THETA2_HPD<-HPDinterval(as.mcmc(chain$Theta2), prob=0.95)
 
-VAR_auto_corr
-VAR_EffectiveSS
-VAR_HPD
+S2_auto_corr<-acf(chain$S2, plot=FALSE)
+S2_EffectiveSS<-effectiveSize(chain$S2)
+S2_HPD<-HPDinterval(as.mcmc(chain$S2), prob=0.95)
 
-K_table
+
+pX1<-sum(chain[chain$x1==1]/length(chain$x1)
+pX2<-sum(chain[chain$x2==1]/length(chain$x2)
+
+
+P_auto_corr
+P_EffectiveSS
+P_HPD
+
+THETA1_auto_corr
+THETA1_EffectiveSS
+THETA1_HPD
+
+S1_auto_corr
+S1_EffectiveSS
+S1_HPD<
+
+THETA2_auto_corr
+THETA2_EffectiveSS
+THETA2_HPD
+
+S2_auto_corr
+S2_EffectiveSS
+S2_HPD
+
+
+pX1
+pX2
