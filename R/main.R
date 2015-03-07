25b83a496d10e8e989de5eb9457804a179bcc76f
Jacob Carey <jacobcvt12@gmail.com>
2015-03-07 15:59:21 -0500

    Fix merge

--- R/main.R
+++ R/main.R
@@ -2,13 +2,6 @@
 
 # Mixture model of two component univariate mixture of normals
 
-<<<<<<< HEAD
-# libraries
-library(Rcpp)
-
-# simulated data
-y <- 0.3*rnorm(100, 0, 1) + 0.7*rnorm(100, 3, 2)
-=======
 # read in data
 #y <- scan("data/simulated.dat")
 simulateZ <- function(N, p){
@@ -30,7 +23,6 @@ theta <- c(0, 2)
 sigma <- c(1, 3)
 z <- simulateZ(100, p)
 y <- rnorm(100, theta[z], sigma[z])
->>>>>>> multisource
 
 # priors
 # thetas ~ N(mu, tau^2)
@@ -39,35 +31,21 @@ y <- rnorm(100, theta[z], sigma[z])
 
 # theta priors
 mu.0 <- 0
-<<<<<<< HEAD
-tau.20 <- 10
-=======
 tau.20 <- 5
->>>>>>> multisource
 
 # s priors
 sigma.20 <- 1
 v.0 <- 5
 
 # p priors
-<<<<<<< HEAD
-a <- 1
-b <- 3
-=======
 a <- 2
 b <- 1
->>>>>>> multisource
 
 # load C++ MCMC sampler
 #sourceCpp("src/sampler.cpp")
 
 # number of iterations
 S <- 1e5
-<<<<<<< HEAD
-
-# explore joint posterior
-PHI <- sampler(y, mu.0, tau.20, sigma.20, v.0, a, b, S)
-=======
 B <- 1e4
 
 # explore joint posterior
@@ -75,7 +53,6 @@ set.seed(42)
 #library(coda)
 #PHI <- sampler(y, mu.0, tau.20, sigma.20, v.0, a, b, S, B)[(B+1):(B+S), ]
 #plot(mcmc(PHI[, 1:5]))
->>>>>>> multisource
 
 # drop burnins
 PHI <- PHI[1001:nrow(PHI), ]
--- src/sampler.cpp
+++ src/sampler.cpp
@@ -4,11 +4,7 @@
 #include <map>
 #include <numeric>
 
-<<<<<<< HEAD
-#define ACCEPTTRACK 100
-=======
 #include "track_proposals.h"
->>>>>>> multisource
 
 // [[Rcpp::export]]
 Rcpp::NumericMatrix sampler(Rcpp::NumericVector y, 
@@ -28,18 +24,6 @@ Rcpp::NumericMatrix sampler(Rcpp::NumericVector y,
     PHI(0, 0) = p;
 
     double theta1= R::rnorm(mu0, sqrt(tau20));
-<<<<<<< HEAD
-    PHI(0, 1) = theta1;
-
-    double s1 = R::rgamma(v0/2, 2/(v0*sigma20));
-    PHI(0, 2) = s1;
-
-    double theta2 = R::rnorm(mu0, sqrt(tau20));
-    PHI(0, 3) = theta2;
-
-    double s2 = R::rgamma(v0/2, 2/(v0*sigma20));
-    PHI(0, 4) = s1;
-=======
     //double theta1 = 0;
     PHI(0, 1) = theta1;
 
@@ -57,10 +41,6 @@ Rcpp::NumericMatrix sampler(Rcpp::NumericVector y,
 
     // NB: theta's and s's should be changed to vectors
     // instead of individual variables
->>>>>>> multisource
-
-    // NB: theta's and s's should be changed to vectors
-    // instead of individual variables
 
     // intial x's~binom(0.5)
     Rcpp::NumericVector x_s = Rcpp::rbinom(y.length(), 1, 0.3);
@@ -69,19 +49,6 @@ Rcpp::NumericMatrix sampler(Rcpp::NumericVector y,
     }
 
     // initialize value for delta
-<<<<<<< HEAD
-    std::map<std::string, float> delta;
-    delta.insert(std::make_pair("p", 0.1));
-    delta.insert(std::make_pair("theta1", 1));
-    delta.insert(std::make_pair("s1", 0.2));
-    delta.insert(std::make_pair("theta2", 1));
-    delta.insert(std::make_pair("s2", 0.2));
-
-    // begin metropolis routine
-    for (int s = 1; s < S + B; ++s) {
-        if (s % 100 == 0) {
-            Rcpp::Rcout << s << std::endl;
-=======
     track_acceptance delta;
     delta.track_parameter("p", 0.1);
     delta.track_parameter("theta1", 1);
@@ -99,7 +66,6 @@ Rcpp::NumericMatrix sampler(Rcpp::NumericVector y,
             if (s % 100 == 0) {
                 delta.modify_deltas();
             }
->>>>>>> multisource
         }
 
         // include only y's where corresponding x
@@ -116,16 +82,12 @@ Rcpp::NumericMatrix sampler(Rcpp::NumericVector y,
         Rcpp::NumericVector x_s_star(x_s.length());
 
         for (int i = 0; i < x_s.length(); i++) {
-<<<<<<< HEAD
-            x_s_star(i) = int (x_s(i) + 1) % 2;
-=======
             if (R::runif(0, 1) < delta["x" + std::to_string (i)]) {
                 x_s_star(i) = int (x_s(i) + 1) % 2;
             } else {
                 x_s_star(i) = x_s(i);
                 delta.accept_reject("x" + std::to_string(i), 0);
             }
->>>>>>> multisource
         }
 
         // accept or reject candidate
@@ -140,12 +102,9 @@ Rcpp::NumericMatrix sampler(Rcpp::NumericVector y,
 
         if (log(R::runif(0, 1)) < log_r) {
             theta1 = theta1_star;
-<<<<<<< HEAD
-=======
             delta.accept_reject("theta1", 1);
         } else {
             delta.accept_reject("theta1", 0);
->>>>>>> multisource
         }
         
         // s1 log acceptance ratio
@@ -206,17 +165,6 @@ Rcpp::NumericMatrix sampler(Rcpp::NumericVector y,
 
         // x log acceptance ratio
         for (int i = 0; i < x_s.length(); i++) {
-<<<<<<< HEAD
-            log_r = R::dnorm(y(i), theta2, 1/sqrt(s2), true) -
-                    R::dnorm(y(i), theta1, 1/sqrt(s1), true);
-            if (x_s_star(i) == 1) {
-                log_r = -1 * log_r;
-            }
-
-            if (log(R::runif(0, 1)) < log_r) {
-                x_s(i) = x_s_star(i);
-            }
-=======
             if (x_s(i) != x_s_star(i)) {
                 if (int (x_s(i)) == 0) {
                     log_r = R::dnorm(y(i), theta1, 1/sqrt(s1), true) +
@@ -237,7 +185,6 @@ Rcpp::NumericMatrix sampler(Rcpp::NumericVector y,
                     delta.accept_reject("x" + std::to_string(i), 0);
                 }
             } 
->>>>>>> multisource
         }
 
         // update PHI
